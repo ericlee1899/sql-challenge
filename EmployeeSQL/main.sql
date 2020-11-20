373ab3cb2d147006930857b127e68e0a09f71713
ericlee1899 <46763482+ericlee1899@users.noreply.github.com>
2020-11-19 23:55:35 -0500

    Add files via upload

--- EmployeeSQL/main.sql
+++ EmployeeSQL/main.sql
@@ -1,1 +1,128 @@
+-- creating tables
+CREATE TABLE "departments" (
+    "dept_no" VARCHAR NOT NULL,
+    "dept_name" VARCHAR NOT NULL,
+    CONSTRAINT "dep_pk" PRIMARY KEY ("dept_no")
+);
 
+CREATE TABLE "dept_emp" (
+    "emp_no" INT NOT NULL,
+    "dept_no" VARCHAR NOT NULL
+);
+
+CREATE TABLE "dept_manager" (
+    "dept_no" VARCHAR NOT NULL,
+    "emp_no" INT NOT NULL
+);
+
+CREATE TABLE "employees" (
+    "emp_no" INT NOT NULL,
+	"emp_title" VARCHAR NOT NULL,
+    "birth_date" VARCHAR NOT NULL,
+    "first_name" VARCHAR NOT NULL,
+    "last_name" VARCHAR NOT NULL,
+    "sex" VARCHAR NOT NULL,
+    "hire_date" VARCHAR NOT NULL,
+    CONSTRAINT "emp_pk" PRIMARY KEY ("emp_no")
+);
+
+CREATE TABLE "salaries" (
+    "emp_no" INT NOT NULL,
+    "salary" INT NOT NULL
+);
+
+CREATE TABLE "titles" (
+    "emp_title" VARCHAR NOT NULL,
+    "title" VARCHAR NOT NULL,
+	CONSTRAINT "titles_pk" PRIMARY KEY ("emp_title")
+);
+
+ALTER TABLE "dept_emp" ADD CONSTRAINT "dept_emp_no_fk" FOREIGN KEY("dept_no")
+REFERENCES "departments" ("dept_no");
+
+ALTER TABLE "dept_manager" ADD CONSTRAINT "dept_man_no_fk" FOREIGN KEY("dept_no")
+REFERENCES "departments" ("dept_no");
+
+ALTER TABLE "dept_emp" ADD CONSTRAINT "emp_no_emp_fk" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "dept_manager" ADD CONSTRAINT "emp_no_man_fk" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "salaries" ADD CONSTRAINT "emp_no_sal_fk" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "employees" ADD CONSTRAINT "title_fk" FOREIGN KEY("emp_title")
+REFERENCES "titles" ("emp_title");
+
+-- testing tables
+SELECT * FROM departments;
+SELECT * FROM dept_emp;
+SELECT * FROM dept_manager;
+SELECT * FROM employees;
+SELECT * FROM salaries;
+SELECT * FROM titles;
+
+-- data analysis --
+-- q1
+SELECT employees.emp_no, employees.last_name, employees.first_name, employees.sex, salaries.salary
+FROM employees
+JOIN salaries
+ON salaries.emp_no = employees.emp_no;
+
+-- q2
+SELECT first_name, last_name, hire_date 
+FROM employees
+WHERE hire_date like '%1986';
+
+-- q3
+SELECT departments.dept_no, departments.dept_name, dept_manager.emp_no, employees.last_name, employees.first_name
+FROM departments
+JOIN dept_manager
+ON dept_manager.dept_no = departments.dept_no
+JOIN employees
+ON employees.emp_no = dept_manager.emp_no;
+
+-- q4
+SELECT dept_emp.emp_no, employees.last_name, employees.first_name, departments.dept_name
+FROM dept_emp
+JOIN employees
+ON employees.emp_no = dept_emp.emp_no
+JOIN departments
+ON departments.dept_no = dept_emp.dept_no;
+
+-- q5
+SELECT first_name, last_name, sex
+FROM employees
+WHERE first_name = 'Hercules'
+AND last_name LIKE 'B%';
+
+-- q6
+SELECT dept_emp.emp_no, employees.last_name, employees.first_name, departments.dept_name
+FROM dept_emp
+JOIN employees
+ON employees.emp_no = dept_emp.emp_no
+JOIN departments
+ON departments.dept_no = dept_emp.dept_no
+WHERE departments.dept_name = 'Sales';
+
+-- q7
+SELECT dept_emp.emp_no, employees.last_name, employees.first_name, departments.dept_name
+FROM dept_emp
+JOIN employees
+ON employees.emp_no = dept_emp.emp_no
+JOIN departments
+ON departments.dept_no = dept_emp.dept_no
+WHERE departments.dept_name = 'Development' 
+OR departments.dept_name = 'Sales';
+
+-- q8
+SELECT last_name, COUNT(last_name)
+FROM employees
+GROUP BY last_name
+ORDER BY COUNT(last_name) DESC;
+
+--epilogue
+SELECT *
+FROM employees
+WHERE emp_no = 499942;
